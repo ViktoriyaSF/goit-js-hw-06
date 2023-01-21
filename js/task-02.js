a8e526fc7c3d6aeb0ef844c99427bc838ee4277d
ViktoriyaSF <kievstar01051985@gmail.com>
2023-01-21 22:55:13 +0200

    task_02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector("ul#ingredients");
+
+const itemsAll = ingredients.map((ingredient) => {
+  const itemEL = document.createElement("li");
+  itemEL.textContent = ingredient;
+  itemEL.classList.add("item");
+  return itemEL;
+});
+
+listEl.append(...itemsAll);

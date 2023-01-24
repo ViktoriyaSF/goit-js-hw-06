c2f5da7bcf7a88da84c3e296ba8ed12922e4387e
ViktoriyaSF <kievstar01051985@gmail.com>
2023-01-24 18:50:04 +0200

    task-01

--- js/task-01.js
+++ js/task-01.js
@@ -8,7 +8,7 @@ console.log(`Number of categories: `, numberCategories.length);
 // 2 варіант
 numberCategories.forEach((element) => {
   console.log("Category:", element.querySelector("h2").textContent);
-  console.log("Category:", element.querySelectorAll("li").length);
+  console.log("Elements:", element.querySelectorAll("li").length);
 });
 
 //1 варіант
@@ -17,15 +17,15 @@ numberCategories.forEach((element) => {
 // const titleFirstEl = firstEl.querySelector("h2");
 // const itemFistEl = firstEl.querySelectorAll("li");
 
-// console.log(`Category:`, firstEl.querySelector("h2").textContent);
-// console.log(`Elements:`, firstEl.querySelectorAll("li").length);
+// console.log("Category:", firstEl.querySelector("h2").textContent);
+// console.log("Elements:", firstEl.querySelectorAll("li").length);
 
 // const secondEl = allCategories.children[1];
 
-// console.log(`Category:`, secondEl.querySelector("h2").textContent);
-// console.log(`Elements:`, secondEl.querySelectorAll("li").length);
+// console.log("Category:", secondEl.querySelector("h2").textContent);
+// console.log("Elements:", secondEl.querySelectorAll("li").length);
 
 // const lastEl = allCategories.lastElementChild;
 
-// console.log(`Category:`, lastEl.querySelector("h2").textContent);
-// console.log(`Elements:`, lastEl.querySelectorAll("li").length);
+// console.log("Category:", lastEl.querySelector("h2").textContent);
+// console.log("Elements:", lastEl.querySelectorAll("li").length);

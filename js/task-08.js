39cafeef8ba79423232111b08ea5750825a62fb4
ViktoriyaSF <kievstar01051985@gmail.com>
2023-02-02 00:01:59 +0200

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector("form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.peaventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  console.log(event.currentTarget);
+  if (email.value === "" || password.value === "") {
+    return alert("Попредження! Всі поля повинні бути заповненні");
+  } else {
+    const formData = new FormData(event.currentTarget);
+    formData.forEach((value, name) => {
+      console.log("value:", value);
+      console.log("name:", name);
+    });
+  }
+}

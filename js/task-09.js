fa4892922cec73c0210f7c6e08bbb9b908f7a20c
ViktoriyaSF <kievstar01051985@gmail.com>
2023-02-02 21:47:32 +0200

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+console.log(getRandomHexColor());
+
+const changeColor = document.querySelector("span.color");
+const changeColorBtn = document.querySelector(".change-color");
+
+changeColorBtn.addEventListener("click", () => {
+  changeColor.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = getRandomHexColor();
+  changeColor.style.backgroundColor = "#fafafa";
+  changeColor.style.borderRadius = "25%";
+});
